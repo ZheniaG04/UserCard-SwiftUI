1a1a8dc4ce7bdf232b683a7ff39b98c1687214a5
ZheniaG04 <evhenguts@gmail.com>
2021-01-07 17:21:55 +0200

    Initial Commit

---
+++ UserCard/UserCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserCardApp.swift
+//  UserCard
+//
+//  Created by Женя on 07.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct UserCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
